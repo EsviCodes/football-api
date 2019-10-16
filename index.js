62865ca83b08a1635ae13ac2fc8847e4144892a5
EsviCodes <esther.js.vis@gmail.com>
2019-10-16 15:28:40 +0200

    Set-up Express app

---
+++ index.js
@@ -0,0 +1,11 @@
+// Import express.
+const express = require("express");
+
+// Declare a constant named app and set it to the output of the express function.
+const app = express();
+
+// Declare a constant named port equal to the process.env.PORT if it is defined. If it is not defined, use the number 4000, like const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
+
+// Pass the port and a logging function to app.listen to start the server.
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));

ef9ca888acbce7d03c61198e0122f00b515c28ee
EsviCodes <esther.js.vis@gmail.com>
2019-10-17 15:49:17 +0200

    Installed and added cors

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
-// Import express.
 const express = require("express");
-// const db = require("./db");
-//const Team = require("./team/model");
+const cors = require("cors");
 const teamRouter = require("./team/router");
 const playerRouter = require("./player/router");
 const bodyParser = require("body-parser");
-//const Player = require("./player/model");
 
-// Declare a constant named app and set it to the output of the express function.
 const app = express();
 const jsonParser = bodyParser.json();
+const corsMiddleware = cors();
 
-// Declare a constant named port equal to the process.env.PORT if it is defined. If it is not defined, use the number 4000, like const port = process.env.PORT || 4000
 const port = process.env.PORT || 4000;
 
 // Middleware
-//app.use(bodyParser.urlencoded({ extended: false })); // not mandotary
+app.use(corsMiddleware);
 app.use(jsonParser); // USE THESE BEFORE THE ROUTE!
 app.use(teamRouter);
 app.use(playerRouter);
 
-// Pass the port and a logging function to app.listen to start the server.
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-/* 
-You can also do:
-app
-.use(//)
-.listen(//)
-*/

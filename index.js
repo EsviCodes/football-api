f260ae8d847b207d5a0c81af7c0a5a6f21892567
EsviCodes <esther.js.vis@gmail.com>
2019-10-17 08:53:26 +0200

    Added test data to database

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // Import express.
 const express = require("express");
 // const db = require("./db");
-// const Team = require("./team/model");
+const Team = require("./team/model"); // team wasn't defined without this one, even though we use it in teamRouter
 const teamRouter = require("./team/router");
+const bodyParser = require("body-parser");
+
+const jsonParser = bodyParser.json();
 
 // Declare a constant named app and set it to the output of the express function.
 const app = express();
@@ -11,13 +14,21 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 // Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(teamRouter);
+app.use(jsonParser);
+
 // Pass the port and a logging function to app.listen to start the server.
+app.post("/team", (req, res, next) => {
+  Team.create(req.body)
+    //.then(team => res.json({ name: team }))// doesn't work -- object with null as response
+    .then(team => res.json(team)) // doesn't work -- object with null as response
+    .catch(next);
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 /* 
 You can also do:
-
 app
 .use(//)
 .listen(//)
--- team/router.js
+++ team/router.js
@@ -20,5 +20,17 @@ router.get("/team", (request, response, next) => {
     .catch(next);
 });
 
+// THIS DIDN't WORK, BUT IT DID WORK WHEN I USED app.post() IN INDEX.JS ??
+
+// Register a POST endpoint for teams in team/router.js. The route should listen for POST requests on the /team route. Pass the request's body to Team.create, which sequelize will use to populate the row's fields.
+// Add a then callback that sends the newly created team as a the response. Add a catch callback where you pass any caught errors to next.
+
+// router.post("/team", (req, res, next) => {
+//   Team.create(req.body)
+//     //.then(team => res.json({ name: team }))// doesn't work -- object with null as response
+//     .then(team => res.json(team)) // doesn't work -- object with null as response
+//     .catch(next);
+// });
+
 // Export the router.
 module.exports = router;
